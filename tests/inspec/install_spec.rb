de570ad2ec35b7842f220d694cb336e2256a3cef
diodonfrost <antoine.mace@protonmail.com>
2018-08-10 23:22:50 +0200

    add basic sshd config

--- tests/inspec/install_spec.rb
+++ tests/inspec/install_spec.rb
@@ -8,3 +8,13 @@ control 'install-01' do
     it { should be_installed }
   end
 end
+
+control 'install-02' do
+  impact 1.0
+  title 'Openssh-server service'
+  desc 'Openssh-server service should be running'
+  describe service('sshd') do
+    it { should be_running }
+    it { should be_enabled }
+  end
+end
