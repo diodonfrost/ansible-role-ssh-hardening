51aca57e2ca511498c0a7f34934b3e38d06665f4
diodonfrost <antoine.mace@protonmail.com>
2018-10-26 14:42:28 +0200

    modify ansible package nane for kitchen tests on freebsd

--- tests/vagrant/vagrant_freebsd_target.rb
+++ tests/vagrant/vagrant_freebsd_target.rb
@@ -1,6 +1,6 @@
 Vagrant.configure(2) do |config|
   config.vm.provision "shell", inline: <<-SHELL
-     pkg install -y py27-ansible-2.6.0
+     pkg install -y py27-ansible
      ln -s /usr/local/bin/python2.7 /usr/bin/python
   SHELL
 end
